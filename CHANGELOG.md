d1c9b9fb1ab0302d68b14fa08c845ba19a2ad17e
olivia-johnson1017 <ojohnso2@nd.edu>
2022-10-24 10:25:34 -0400

    Parse methods, changelog, url diagram

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.4.0] - 2017-06-20
+### Added
+- Parse methods 
+    - getting all, creating, and deleting grocery items
+    - getting all, creating, and deleting meals
+    - getting all, getting by week, creating, and deleting calendar date/meal pairings
+    - getting all, getting one, creating, and deleting users
+
+### Changed
+
+
+### Removed
+- JSON psuedo database
+- Parse methods for fetching from parse database
--- src/Common/Services/LearnServices.js
+++ src/Common/Services/LearnServices.js
@@ -1,7 +1,7 @@
 import Parse from "parse";
 /* SERVICE FOR PARSE SERVER OPERATIONS */
 
-// READ operation - get all items in Parse class item
+// Read operation for groceries
 export const getAllGroceries = () => {
   const Item = Parse.Object.extend("Groceries");
   const query = new Parse.Query(Item);
@@ -12,6 +12,7 @@ export const getAllGroceries = () => {
   });
 };
 
+// Create operation for groceries
 export const createItem = (itemName, Quantity) => {
   console.log("Creating: ", itemName);
   const Item = Parse.Object.extend("Groceries");
@@ -25,9 +26,140 @@ export const createItem = (itemName, Quantity) => {
   });
 };
 
-// DELETE operation - remove lesson by ID
+// Delete operation for groceries
 export const removeItem = (id) => {
   const Item = Parse.Object.extend("Groceries");
+  const query = new Parse.Query(Item);
+  return query.get(id).then((item) => {
+    item.destroy();
+  });
+};
+
+// Read operation for meals
+export const getAllMeals = () => {
+  const Item = Parse.Object.extend("Meals");
+  const query = new Parse.Query(Item);
+  return query.find().then((results) => {
+    // returns array of item objects
+    console.log("results: ", results);
+    return results;
+  });
+};
+
+// Create operation for meals
+export const createMeals = (mealName, servings, recipe) => {
+  console.log("Creating: ", mealName);
+  const Item = Parse.Object.extend("Meals");
+  const item = new Item();
+  // using setter to UPDATE the object
+  item.set("mealName", mealName);
+  item.set("servings", servings);
+  item.set("recipe", recipe)
+  return item.save().then((result) => {
+    // returns new Lesson object
+    return result;
+  });
+};
+
+// Delete operation for meals
+export const removeMeals = (id) => {
+  const Item = Parse.Object.extend("Meals");
+  const query = new Parse.Query(Item);
+  return query.get(id).then((item) => {
+    item.destroy();
+  });
+};
+
+// Read operation for calendar
+export const getFullCalendar = () => {
+  const Item = Parse.Object.extend("Calendar");
+  const query = new Parse.Query(Item);
+  return query.find().then((results) => {
+    // returns array of item objects
+    console.log("results: ", results);
+    return results;
+  });
+};
+
+// Secondary read operation for calendar - returns data for the current week
+export const getCurrentWeek = () => {
+  const today = newDate();
+  const dayOfWeek = today.getDay();
+  const startDate = newDate(today.getDate() - dayOfWeek);
+  const endDate = newDate(today.getDate() + (6 - dayOfWeek));
+  const Item = Parse.Object.extend("Calendar");
+  const query = new Parse.Query(Item);
+  query.greaterThanOrEqualTo('date', startDate);
+  query.lessThanOrEqualTo('date', endDate);
+  return query.find().then((results) => {
+    // returns array of item objects
+    console.log("results: ", results);
+    return results;
+  });
+};
+
+// Create operation for calendar
+export const createDate = (date, meal) => {
+  console.log("Creating: ", date);
+  const Item = Parse.Object.extend("Calendar");
+  const item = new Item();
+  // using setter to UPDATE the object
+  item.set("date", date);
+  item.set("meal", meal);
+  return item.save().then((result) => {
+    // returns new Lesson object
+    return result;
+  });
+};
+
+// Delete operation for calendar
+export const removeCalendar = (id) => {
+  const Item = Parse.Object.extend("Calendar");
+  const query = new Parse.Query(Item);
+  return query.get(id).then((item) => {
+    item.destroy();
+  });
+};
+
+// Read operation for users
+export const getAllUsers = () => {
+  const Item = Parse.Object.extend("Users");
+  const query = new Parse.Query(Item);
+  return query.find().then((results) => {
+    // returns array of item objects
+    console.log("results: ", results);
+    return results;
+  });
+};
+
+// Secondary read operation for users (returns one user by id)
+export const getUser = (id) => {
+  const Item = Parse.Object.extend("Users");
+  const query = new Parse.Query(Item);
+  return query.get(id).then((item) => {
+    console.log("result: ", item);
+    return item;
+  });
+};
+
+// Create operation for users
+export const createUser = (username, email, password) => {
+  console.log("Creating: ", username);
+  const Item = Parse.Object.extend("Users");
+  const item = new Item();
+  // using setter to UPDATE the object
+  item.set("userame", username);
+  item.set("email", email);
+  item.set("password", password)
+  return item.save().then((result) => {
+    // returns new Lesson object
+    return result;
+  });
+};
+
+// Delete operation for users
+export const removeUser = (id) => {
+  const Item = Parse.Object.extend("Users");
   const query = new Parse.Query(Item);
   return query.get(id).then((item) => {
     item.destroy();
