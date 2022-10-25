0f605dc5a3a7317edd58e9b58f9205b9fe476d22
lpierson23 <78287653+lpierson23@users.noreply.github.com>
2022-10-25 16:22:38 -0400

    Update ShoppingList.js

--- src/Components/Shopping/ShoppingList.js
+++ src/Components/Shopping/ShoppingList.js
@@ -8,15 +8,16 @@ import ShoppingForm from "./ShoppingForm";
 
 /* STATEFUL PARENT COMPONENT */
 const ShoppingList = () => {
+  const [newItem, setNewItem] = useState({
+    itemName: "",
+    quantity: ""
+  });
+
   // Variables in the state to hold data
   const [items, setItems] = useState([]);
-  //const [item, setItem] = useState([]);
-  const [item_name, setItemName] = useState();
-  const [quantity, setQuantity] = useState();
-  // const [state, setState] = useState({
-  //   item_name: "",
-  //   quantity: "1"
-  // });
+  // Flags in the state to watch for add/remove updates
+  const [add, setAdd] = useState(false);
+  const [remove, setRemove] = useState("");
 
   // UseEffect to run when the page loads to
   // obtain async data and render
@@ -27,35 +28,31 @@ const ShoppingList = () => {
     });
   }, []);
 
-  // Flags in the state to watch for add/remove updates
-  const [add, setAdd] = useState(false);
-  const [remove, setRemove] = useState("");
-
-  // UseEffect that runs when changes
-  // are made to the state variables/flags
   useEffect(() => {
-    // Check for add flag and make sure name state variable is defined
-    if (item_name && quantity && add) {
-      // console.log("name: ", item_name);
-      createItem(item_name, quantity).then((newItem) => {
+    if (newItem && add) {
+      createItem(newItem).then((itemCreated) => {
+        if (itemCreated) {
+          alert(`${itemCreated.get("itemName")} successfully added to list!`);
+        }
+        // TODO: redirect user to main app
         setAdd(false);
-        setItems([...items, newItem]);
       });
     }
+  }, [newItem, add]);
 
-    // Check if remove state variable is holding an ID
-    if (remove.length > 0) {
-      //Filter the old items list to take out selected item
-      const newItems = items.filter((item) => item.id !== remove);
-      setItems(newItems);
+  // Check if remove state variable is holding an ID
+  //   if (remove.length > 0) {
+  //     //Filter the old items list to take out selected item
+  //     const newItems = items.filter((item) => item.id !== remove);
+  //     setItems(newItems);
 
-      removeItem(remove).then(() => {
-        console.log("Removed item with ID: ", remove);
-      });
-      // Reset remove state variable
-      setRemove("");
-    }
-  }, [item_name, quantity, items, add, remove]);
+  //     removeItem(remove).then(() => {
+  //       console.log("Removed item with ID: ", remove);
+  //     });
+  //     // Reset remove state variable
+  //     setRemove("");
+  //   }
+  // }, [add, remove]);
 
   // Handler to handle event passed from child submit button
   const onClickHandler = (e) => {
@@ -68,19 +65,31 @@ const ShoppingList = () => {
   // Handler to track changes to the child input text
   const onChangeHandler = (e) => {
     e.preventDefault();
-    // const value = e.target.value;
-    // setState({
-    //   ...state,
-    //   [e.target.name]: value
-    // });
+    const { name, value: newValue } = e.target;
+    console.log(newValue);
 
-    // setItemName(e.target.value);
-    // setQuantity(e.target.value);
+    setNewItem({
+      ...newItem,
+      [name]: newValue
+    });
+    console.log("onChange:", newValue);
   };
+
+  // Handler to track changes to the child input text
+  // const onQuantityChangeHandler = (e) => {
+  //   e.preventDefault();
+  //   console.log("onQuantityChange:", e.target.value);
+  //   setQuantity(e.target.value);
+  // };
 
   return (
     <div>
-      <ShoppingForm onClick={onClickHandler} onChange={onChangeHandler} />
+      <ShoppingForm
+        onClick={onClickHandler}
+        onChange={onChangeHandler}
+        item={newItem}
+        // onQuantityChange={onChangeHandler}
+      />
       <div>
         <hr />
         <h3>Shopping List</h3>
@@ -88,7 +97,7 @@ const ShoppingList = () => {
           <ul>
             {items.map((item) => (
               <li key={item.id}>
-                {item.get("item_name")} ({item.get("quantity")})
+                {item.get("itemName")} ({item.get("quantity")})
               </li>
             ))}
           </ul>
