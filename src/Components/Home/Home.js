244b6569d04c8e0bd5f9c9730ce96ee3dbfad042
lpierson23 <lpierson@nd.edu>
2022-11-03 16:34:45 -0400

    home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,10 +10,13 @@ const Home = () => {
   
         <h3>You can use this website to plan out meals with your roommates!</h3>
         <p>
-          Use the links above to add a meal request, add items to your shopping
+          Please log in or register to access links to add a meal request, add items to your shopping
           list, and view your calendar of meals.
         </p>
         {/* <img src= plates.jpg width="300" height="150" /> */}
+
+        <AuthModule/>
+
       </div>
     );
   };
