e1e94aed292a5baa64a9f8ac6c526de1acc6b142
olivia-johnson1017 <ojohnso2@nd.edu>
2022-12-01 09:24:39 -0500

    uncommenting iframe

--- src/Components/RecipeBook/RecipeView.js
+++ src/Components/RecipeBook/RecipeView.js
@@ -24,7 +24,7 @@ const RecipeView = ({ mealId }) => {
                     <h2 className="recipe-heading"> {meal.attributes.mealName} </h2>
                     <p className="recipe-text"> Servings: {meal.attributes.servings} </p>
                     {/* TODO: fix errors with iframe */}
-                    {/* {meal.attributes.url && (<iframe src={meal.attributes.url} width="50%" height="400px" title="Recipe"></iframe>)} */}
+                    {meal.attributes.url && (<iframe src={meal.attributes.url} width="50%" height="400px" title="Recipe"></iframe>)}
                     {!meal.attributes.url && (<p className="recipe-text">No recipe given</p>)}
                     <br />
                     <br />
