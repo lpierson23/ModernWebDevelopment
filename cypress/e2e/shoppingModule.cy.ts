84899acc7753020e2a9d3bf643dac3d8bb5d0848
olivia-johnson1017 <ojohnso2@nd.edu>
2022-12-01 16:28:26 -0500

    adding base cypress files

---
+++ cypress/e2e/shoppingModule.cy.ts
@@ -0,0 +1,47 @@
+describe('protected routing', () => {
+    it("has a protected routing and auto-rerouting to auth module", () => {
+        cy.visit("/shopping");
+        cy.hash().should("eq", "#/");
+    });
+  });
+  
+describe("shopping functionality", () => {
+    beforeEach(() => {
+        cy.visit("/login");
+        cy.get("[data-test=email]").type("fake@email.com");
+        cy.get("[data-test=password]").type("password{enter}");
+        cy.get('button[type="submit"]').click();
+        cy.visit("/shopping");
+    });
+
+    it("contains nav bar", () => {
+        cy.contains("#navbar");
+    });
+
+    it("requires grocery item field", () => {
+        cy.get('button[type="submit"]').click();
+        cy.get(".error-messages").should("contain", "itemName can't be blank");
+    });
+
+    it("requires servings field", () => {
+        cy.get("[data-test=itemName]").type("spinach");
+        cy.get('button[type="submit"]').click();
+        cy.get(".error-messages").should("contain", "quantity can't be blank");
+    });
+
+    it("able to submit form", () => {
+        cy.get("[data-test=itemName]").type("spinach");
+        cy.get("[data-test=quantity]").type("4");
+        cy.get('button[type="submit"]').click();
+        cy.on('window:alert',(t)=>{
+        expect(t).to.contains('spinach successfully added to list!');
+        })
+    });
+
+    it("displays list of current grocery items", () => {
+        cy.get("[data-test=itemName]").type("spinach");
+        cy.get("[data-test=quantity]").type("4");
+        cy.get('button[type="submit"]').click();
+        cy.contains("#shoppingList li spinach");
+    });
+});
