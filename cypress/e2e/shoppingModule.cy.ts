1e2228e86d5232a7c8d6b70d58c1b2427e052c85
olivia-johnson1017 <ojohnso2@nd.edu>
2022-12-05 12:05:31 -0500

    updating to unit tests

--- cypress/e2e/shoppingModule.cy.ts
+++ cypress/e2e/shoppingModule.cy.ts
@@ -1,11 +1,3 @@
-function preventFormSubmitDefault(selector) {
-    cy.get(selector).then(form$ => {
-        form$.on("submit", e => {
-            e.preventDefault();
-        });
-    });
-}
-
 describe('protected routing', () => {
     it("has a protected routing and auto-rerouting to auth module", () => {
         cy.visit("/shopping");
@@ -16,48 +8,41 @@ describe('protected routing', () => {
 describe("shopping functionality", () => {
     beforeEach(() => {
         cy.visit("/auth/login");
-        // preventFormSubmitDefault("form");
         cy.get("#email-input").type("fake@email.com");
         cy.get("#password-input").type("password");
         cy.get('button').click();
         cy.wait(1500);
-        // cy.visit("/shopping");
-        // preventFormSubmitDefault("form");
+        cy.visit("/shopping");
     });
 
     it("contains nav bar", () => {
-        cy.visit("/shopping");
         cy.contains("#navbar");
     });
 
-    it("requires grocery item field", () => {
-        cy.visit("/shopping");
-        cy.get('button').click();
-        cy.get(".error-messages").should("contain", "itemName can't be blank");
-    });
+    // it("requires grocery item field", () => {
+    //     cy.get('button').click();
+    //     cy.get(".error-messages").should("contain", "itemName can't be blank");
+    // });
 
-    it("requires servings field", () => {
-        cy.visit("/shopping");
-        cy.get("#itemName").type("spinach");
-        cy.get('button').click();
-        cy.get(".error-messages").should("contain", "quantity can't be blank");
-    });
+    // it("requires servings field", () => {
+    //     cy.get("#itemName").type("spinach");
+    //     cy.get('button').click();
+    //     cy.get(".error-messages").should("contain", "quantity can't be blank");
+    // });
 
-    it("able to submit form", () => {
-        cy.visit("/shopping");
-        cy.get("#itemName").type("spinach");
-        cy.get("#quantity").type("4");
-        cy.get('button').click();
-        cy.on('window:alert',(t)=>{
-        expect(t).to.contains('spinach successfully added to list!');
-        })
-    });
+    // it("able to submit form", () => {
+    //     cy.get("#itemName").type("spinach");
+    //     cy.get("#quantity").type("4");
+    //     cy.get('button').click();
+    //     cy.on('window:alert',(t)=>{
+    //     expect(t).to.contains('spinach successfully added to list!');
+    //     })
+    // });
 
-    it("displays list of current grocery items", () => {
-        cy.visit("/shopping");
-        cy.get("#itemName").type("spinach");
-        cy.get("#quantity").type("4");
-        cy.get('button').click();
-        cy.contains("#shoppingList li spinach");
-    });
+    // it("displays list of current grocery items", () => {
+    //     cy.get("#itemName").type("spinach");
+    //     cy.get("#quantity").type("4");
+    //     cy.get('button').click();
+    //     cy.contains("#shoppingList li spinach");
+    // });
 });
