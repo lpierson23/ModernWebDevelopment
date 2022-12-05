0d978aa355a876296bc19e9b66955bc1495ee560
olivia-johnson1017 <ojohnso2@nd.edu>
2022-12-05 12:00:09 -0500

    updates to unit tests

--- cypress/e2e/shoppingModule.cy.ts
+++ cypress/e2e/shoppingModule.cy.ts
@@ -1,10 +1,10 @@
 function preventFormSubmitDefault(selector) {
     cy.get(selector).then(form$ => {
-      form$.on("submit", e => {
-        e.preventDefault();
-      });
+        form$.on("submit", e => {
+            e.preventDefault();
+        });
     });
-  }
+}
 
 describe('protected routing', () => {
     it("has a protected routing and auto-rerouting to auth module", () => {
@@ -16,30 +16,34 @@ describe('protected routing', () => {
 describe("shopping functionality", () => {
     beforeEach(() => {
         cy.visit("/auth/login");
-        preventFormSubmitDefault("form");
+        // preventFormSubmitDefault("form");
         cy.get("#email-input").type("fake@email.com");
         cy.get("#password-input").type("password");
         cy.get('button').click();
-        cy.visit("/shopping");
-        preventFormSubmitDefault("form");
+        // cy.visit("/shopping");
+        // preventFormSubmitDefault("form");
     });
 
     it("contains nav bar", () => {
+        cy.visit("/shopping");
         cy.contains("#navbar");
     });
 
     it("requires grocery item field", () => {
+        cy.visit("/shopping");
         cy.get('button').click();
         cy.get(".error-messages").should("contain", "itemName can't be blank");
     });
 
     it("requires servings field", () => {
+        cy.visit("/shopping");
         cy.get("#itemName").type("spinach");
         cy.get('button').click();
         cy.get(".error-messages").should("contain", "quantity can't be blank");
     });
 
     it("able to submit form", () => {
+        cy.visit("/shopping");
         cy.get("#itemName").type("spinach");
         cy.get("#quantity").type("4");
         cy.get('button').click();
@@ -49,6 +53,7 @@ describe("shopping functionality", () => {
     });
 
     it("displays list of current grocery items", () => {
+        cy.visit("/shopping");
         cy.get("#itemName").type("spinach");
         cy.get("#quantity").type("4");
         cy.get('button').click();
