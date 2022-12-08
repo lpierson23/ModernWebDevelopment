5dca9f8149553efba0b6a205029964a4ca888626
lpierson23 <lpierson@nd.edu>
2022-12-08 13:12:40 -0500

    Changed back to original database

--- src/environments.js
+++ src/environments.js
@@ -1,5 +1,5 @@
 module.exports = {
-  APPLICATION_ID: "xRgNOr4LNSKwyX6GTIUDZjzNSQwbrEcUpnuiKXY0",
-  JAVASCRIPT_KEY: "gsYEEFm1ZDQhsvG5m2TYA3ITlwqC0rNS8Gbbcwm2",
+  APPLICATION_ID: "hKqtPttXLLBhg7xRYmk0c6KfuV7e8GkscDrj8nHl",
+  JAVASCRIPT_KEY: "zU8I0qorzkyFYauLynkecFLeVGFoYfk5PSqW7Nje",
   SERVER_URL: "https://parseapi.back4app.com/"
 };
